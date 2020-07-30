0a175a7d18753bcb4e23530b95d014586bbecd59
Jurek Kunicki <jerzy.kunicki@gmail.com>
2020-07-30 20:12:14 +0200

    commit2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import './App.min.css';
 class App extends React.Component {
-  state = { 
-    questions: [{
-      id: 1,
-      question: 'co to jest ?',
-      answer1: 'statek',
-      answer2: 'samochód',
-      corectAnswer: 'czołg'
-    },
-    {
-      id: 23,
-      question: 'co to jest ?',
-      answer1: 'statek',
-      answer2: 'samochód',
-      corectAnswer: 'helikopter'
-    }]
-   }
+  constructor(props){
+    super(props);
+    this.state = { 
+      questions: [{
+        id: 1,
+        question: 'co to jest ?',
+        answer1: 'statek',
+        answer2: 'samochód',
+        corectAnswer: 'czołg'
+      },
+      {
+        id: 23,
+        question: 'co to jest ?',
+        answer1: 'statek',
+        answer2: 'samochód',
+        corectAnswer: 'helikopter'
+      }]
+     }
+  }
+  
   render() { 
     const copyQuestions = [...this.state.questions];
     return ( 
