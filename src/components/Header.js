2fdabe16e2dd0c7bf56f006713747e1dfd609025
Jurek Kunicki <jerzy.kunicki@gmail.com>
2020-08-09 22:12:48 +0200

    layout corection

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 const Header = () => {
    
     return ( 
-        <h1>quiz - fizyka niezbyt zaawansowana</h1>
+        <h3>quiz - fizyka niezbyt zaawansowana</h3>
      );
 }
  
